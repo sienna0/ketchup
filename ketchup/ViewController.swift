137d77bea1fad094210c932dcbe3bee3a6f7518b
Sienna Li <sl3493@cornell.edu>
2025-01-07 20:14:20 -0800

    Initial Commit

---
+++ ketchup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ketchup
+//
+//  Created by Sienna Li on 1/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
