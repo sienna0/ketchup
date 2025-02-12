19df704be6726dac5b2b296aaf6c15fa00fe57a5
sienna0 <applerx@gmail.com>
2025-02-12 12:55:20 -0500

    Initial Commit

---
+++ ketchup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ketchup
+//
+//  Created by Sienna Li on 2/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ketchupTests/ketchupTests.swift
@@ -0,0 +1,17 @@
+//
+//  ketchupTests.swift
+//  ketchupTests
+//
+//  Created by Sienna Li on 2/12/25.
+//
+
+import Testing
+@testable import ketchup
+
+struct ketchupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
